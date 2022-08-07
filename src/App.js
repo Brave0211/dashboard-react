ef4c597b2baed00e47f5e942e42f3f4d3e3d7fcf
Brave0211 <abduqodirovjasurbek11@gmail.com>
2022-08-07 23:21:43 +0500

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css"
+import { Sitebar , Header } from "./components";
+import { Home } from "./pages/Home/Home";
+import { Tickets } from "./pages/Tickets/Tickets";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper" >
+      <div className="left" >
+        <Sitebar />
+      </div>
+      <div className="right" >
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/tickets" element={<Tickets/>} />
+        </Routes>
+        </div>
+      </div>
+      </div>
   );
 }
 
